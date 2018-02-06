bcc61af81018a6db524541b4f71cab7ec8ada79e
huggla <henrik.uggla@kristianstad.se>
2018-02-06 12:07:17 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -4,7 +4,7 @@ set -e
 IFS=";"
 smbconf="$CONFIG_DIR/smb.conf"
 mkdir -p "$CONFIG_DIR"
-PASSDB_BACKEND="smbpasswd:\"$SMBPASSWD_FILE\""
+PASSDB_BACKEND="smbpasswd:$SMBPASSWD_FILE"
 if [ -z "$USERNAME_MAP_FILE" ]
 then
    USERNAME_MAP_FILE="$CONFIG_DIR/usermap.txt"
