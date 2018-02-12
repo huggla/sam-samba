1151049dcca31925984f700d2b9405d7fdc7f2a1
huggla <henrik.uggla@kristianstad.se>
2018-02-12 14:27:13 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -4,9 +4,7 @@ set +a
 set +m
 set +s
 set +i
-echo "$1"
-eval "$1"
-env -i /usr/bin/sudo /usr/local/bin/procremount
+#env -i /usr/bin/sudo /usr/local/bin/procremount
 IFS=";"
 smbconf="$CONFIG_DIR/smb.conf"
 env -i /usr/bin/sudo /usr/local/bin/mkdir2root "$SHARES_DIR"
