19171691929a38b6b54bc4d8fc092edc000e2569
huggla <henrik.uggla@kristianstad.se>
2018-02-19 14:13:24 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -69,6 +69,7 @@ then
                exit 1
             fi
          fi
+         echo apa
          env -i $sudo "$SUDO_DIR/chown2root" "$userpwfile"
          env -i $sudo "$SUDO_DIR/addshareuser" "$user" "$userpwfile" "$CONFIG_DIR/smbusers" $DELETE_PASSWORD_FILES
       done
@@ -82,6 +83,7 @@ then
       do
          echo "$user" >> "$global_username_map"
       done
+      echo apa2
       env -i $sudo "$SUDO_DIR/chown2root" -R "$username_dir"
    fi
    echo hej
