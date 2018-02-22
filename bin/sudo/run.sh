95343be98391c8af6991cb40c2d0d36ba8693d64
huggla <henrik.uggla@kristianstad.se>
2018-02-22 11:05:44 +0100

    Update run.sh

--- bin/sudo/run.sh
+++ bin/sudo/run.sh
@@ -20,16 +20,16 @@ then
          return "$(echo $tmp | awk -v param=$2 -F= '$1==param{print $2}')"
       fi
    }
-   IFS="${IFS};"
+#   IFS="${IFS};"
    global_smb_passwd_file="var global smb_passwd_file"
-   smbpasswd_dir="$(dirname "$global_smb_passwd_file")"
-   mkdir -p "$smbpasswd_dir"
-   chmod u=rwx,go= "$smbpasswd_dir"
-   touch "$global_smb_passwd_file"
-   chmod u=rwx,go= "$global_smb_passwd_file"
-   $environment="$environment`echo global_passdb_backend=smbpasswd:$global_smb_passwd_file`"
-   CONFIG_FILE="var - CONFIG_FILE"
-echo $CONFIG_FILE
+ #  smbpasswd_dir="$(dirname "$global_smb_passwd_file")"
+ #  mkdir -p "$smbpasswd_dir"
+ #  chmod u=rwx,go= "$smbpasswd_dir"
+ #  touch "$global_smb_passwd_file"
+ #  chmod u=rwx,go= "$global_smb_passwd_file"
+ #  $environment="$environment`echo global_passdb_backend=smbpasswd:$global_smb_passwd_file`"
+ #  CONFIG_FILE="var - CONFIG_FILE"
+echo $global_smb_passwd_file
 fi
 #exec env -i "$BIN_DIR/runsmbd" "$SUDO_DIR"
 exit 0
