948e09b4592c00deaae96bd3ce7b01d35ecc85e8
huggla <henrik.uggla@kristianstad.se>
2018-02-23 13:54:29 +0100

    Update run.sh

--- bin/sudo/run.sh
+++ bin/sudo/run.sh
@@ -5,12 +5,13 @@ set +m
 set +s
 set +i
 
-readonly SUDO_DIR="`dirname $0`"
+readonly SUDO_DIR="`/usr/bin/dirname $0`"
 readonly ENVIRONMENT_FILE="$SUDO_DIR/environment"
 if [ -f "$ENVIRONMENT_FILE" ]
 then
-   environment=`cat "$ENVIRONMENT_FILE" | /usr/bin/tr -dc '[:alnum:]_ %.=/\n'`
-   rm "$ENVIRONMENT_FILE"
+   IFS=$(echo -en "\n\b,")
+   environment=`/bin/cat "$ENVIRONMENT_FILE" | /usr/bin/tr -dc '[:alnum:]_ %.=/\n'`
+   /bin/rm "$ENVIRONMENT_FILE"
    var(){
       IFS_bak=$IFS
       IFS=?
@@ -18,28 +19,33 @@ then
       then
          tmp="$environment"
       else
-         tmp="$(echo $environment | awk -v section=$1 -F_ '$1==section{s= ""; for (i=2; i < NF; i++) s = s $i "_"; print s $NF}')"
+         tmp="$(echo $environment | /usr/bin/awk -v section=$1 -F_ '$1==section{s= ""; for (i=2; i < NF; i++) s = s $i "_"; print s $NF}')"
       fi
       if [ -z "$2" ]
       then
-         echo "$(echo $tmp | awk -F= '{print $1}')"
+         echo "$(echo $tmp | /usr/bin/awk -F= '{print $1}')"
       else
-         echo "$(echo $tmp | awk -v param=$2 -F= '$1==param{print $2}')"
+         echo "$(echo $tmp | /usr/bin/awk -v param=$2 -F= '$1==param{print $2}')"
       fi
       IFS=$IFS_bak
    }
    readonly CONFIG_FILE="`var - CONFIG_FILE`"
+   readonly SHARES_DIR="`var - SHARES_DIR`"
+   /bin/mkdir -p "$SHARES_DIR"
+   set +e
+   /bin/chown root "$SHARES_DIR"
+   /bin/chmod u=rwx,go=x "$SHARES_DIR"
+   set -e
    if [ ! -s "$CONFIG_FILE" ]
    then
       readonly global_smb_passwd_file="`var global smb_passwd_file`"
       readonly environment=$environment$'\n'"global_passdb_backend=smbpasswd:$global_smb_passwd_file"
       readonly SHARES="global"$'\n'"`var - SHARES`"
-      readonly SHARES_DIR="`var - SHARES_DIR`"
       for share in $SHARES
       do
          echo >> "$CONFIG_FILE"
          echo "[$share]" >> "$CONFIG_FILE"
-         share_lc="$(echo $share | xargs | tr '[:upper:]' '[:lower:]')"
+         share_lc="$(echo $share | /usr/bin/xargs | /usr/bin/tr '[:upper:]' '[:lower:]')"
          share_parameters="`var $share`"
          path_value="$SHARES_DIR/$share"
          for param in $share_parameters
@@ -51,29 +57,31 @@ then
                then
                   path_value=$param_value
                else
-                  echo -n "$param" | tr '_' ' ' >> "$CONFIG_FILE"
+                  echo -n "$param" | /usr/bin/tr '_' ' ' >> "$CONFIG_FILE"
                   echo "=$param_value" >> "$CONFIG_FILE"
                fi
             fi
          done
-         mkdir -p "$path_value"
+         /bin/mkdir -p "$path_value"
          echo "path=$path_value" >> "$CONFIG_FILE"
       done
    else
       readonly environment
-      readonly global_smb_passwd_file="$(echo "$(cat "$CONFIG_FILE" | awk -v param="smb passwd file" -F= '$1==param{print $2}')")"
-      cat "$CONFIG_FILE" | grep '[' | grep -v 'global' | tr '[' '' | tr ']' ''
+      readonly global_smb_passwd_file="$(echo "$(/bin/cat "$CONFIG_FILE" | /usr/bin/awk -v param="smb passwd file" -F= '$1==param{print $2}')")"
+      readonly share_paths="`/bin/cat "$CONFIG_FILE" | /bin/grep 'path=' | /usr/bin/awk -F= '{print $2}'`"
+      for path in $share_paths
+      do
+         /bin/mkdir -p "$path"
+      done
    fi
-   cat "$CONFIG_FILE" | grep '[' | grep -v 'global' | tr '[' '' | tr ']' ''
-   exit
-   smbpasswd_dir="$(dirname "$global_smb_passwd_file")"
-   mkdir -p "$smbpasswd_dir"
-   chmod u=rwx,go= "$smbpasswd_dir"
-   touch "$global_smb_passwd_file"
-   chmod u=rw,go= "$global_smb_passwd_file"
-   SHARE_USERS="`var - SHARE_USERS`"
-   SMBUSERS_FILE="`var - SMBUSERS_FILE`"
-   chmod u=rw,go= "$SMBUSERS_FILE"
+   /bin/mkdir -p "$(/usr/bin/dirname "$global_smb_passwd_file")"
+   set +e
+   /bin/touch "$global_smb_passwd_file"
+   /bin/chown root "$global_smb_passwd_file"
+   /bin/chmod u=rw,go= "$global_smb_passwd_file"
+   set -e
+   readonly SHARE_USERS="`var - SHARE_USERS`"
+   readonly SMBUSERS_FILE="`var - SMBUSERS_FILE`"
    for user in $SHARE_USERS
    do
       if [ ! "`/usr/bin/id $user 2>/dev/null`" ]
@@ -85,17 +93,23 @@ then
    then
       for user in $SHARE_USERS
       do
-         user_lc=$(echo $user | xargs | tr '[:upper:]' '[:lower:]')
+         user_lc=$(echo $user | /usr/bin/xargs | /usr/bin/tr '[:upper:]' '[:lower:]')
          userpwfile="`var - password_file_$user_lc`"
-         if [ -n "$userpwfile" ]
+         if [ -z "$userpwfile" ]
          then
-            chmod u=r,go= "$userpwfile"
-         else
+            userpwfile=$CONFIG_DIR/$user"_pw"
+         fi
+         /bin/mkdir -p "$(/usr/bin/dirname "$userpwfile")"
+         set +e
+         /bin/touch "$userpwfile"
+         /bin/chown root "$userpwfile"
+         /bin/chmod u=rw,go= "$userpwfile"
+         set -e
+         if [ ! -s "$userpwfile" ]
+         then
             user_pw="`var password $user_lc`"
             if [ -n "$user_pw" ]
             then
-               userpwfile=$CONFIG_DIR/$user"_pw"
-               chmod u=r,go= "$userpwfile"
                echo $user_pw > "$userpwfile"
                unset user_pw
             else
@@ -104,33 +118,30 @@ then
             fi
          fi
          echo | /bin/cat "$userpwfile" - "$userpwfile" | "$SUDO_DIR/smbpasswd" -s -a "$user"
+         set +e
+         /bin/rm --interactive=never "$userpwfile"
+         set -e
          echo "$user = $user" >> "$SMBUSERS_FILE"
-         if [ "`var - DELETE_PASSWORD_FILES`" == "yes" ]
-         then
-            /bin/rm "$2"
-fi
-         env -i $sudo "$SUDO_DIR/addshareuser" "$user" "$userpwfile" "$SMBUSERS_FILE" $DELETE_PASSWORD_FILES
       done
    fi
+   readonly global_username_map="`var - global_username_map`"
    if [ -n "$global_username_map" ] 
    then
-      if [ ! -e "$global_username_map" ]
+      /bin/mkdir -p "$(/usr/bin/dirname "$global_username_map")"
+      set +e
+      /bin/touch "$global_username_map"
+      /bin/chown root "$global_username_map"
+      /bin/chmod u=rw,go= "$global_username_map"
+      set -e
+      if [ ! -s "$global_username_map" ]
       then
-         username_dir="$(dirname "$global_username_map")"
-         if [ ! -e "$username_dir" ]
-         then
-            /bin/mkdir -p "$username_dir"
-         fi
-         >"$global_username_map"
+         readonly USERNAME_MAP="`var - USERNAME_MAP`"
          for user in $USERNAME_MAP
          do
             echo "$user" >> "$global_username_map"
          done
       fi
-      env -i $sudo "$SUDO_DIR/chown2root" "$global_username_map"
    fi
-   env -i $sudo "$SUDO_DIR/chown2root" "$CONFIG_DIR"
-   env -i $sudo "$SUDO_DIR/chown2root" "$SHARES_DIR"
 fi
 #exec env -i "$BIN_DIR/runsmbd" "$SUDO_DIR"
 exit 0
