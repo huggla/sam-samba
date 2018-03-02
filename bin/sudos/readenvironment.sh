2432e7c0ef1195250e85c8c69d77a52cb3f27a5e
huggla <henrik.uggla@kristianstad.se>
2018-03-02 10:51:19 +0100

    Update readenvironment.sh

--- bin/sudos/readenvironment.sh
+++ bin/sudos/readenvironment.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 set -e +a +m +s +i -f
 
-readonly SUDOS_DIR="$(/usr/bin/dirname $0)"
-readonly RUNTIME_ENVIRONMENT="$SUDOS_DIR/runtime_environment"
+readonly SUDO_USER
+readonly BIN_DIR="$(/usr/bin/dirname $0)"
+readonly RUNTIME_ENVIRONMENT="$BIN_DIR/runtime_environment"
 if [ -f "$RUNTIME_ENVIRONMENT" ]
 then
-   echo "$SUDO_USER $(/bin/hostname)=(root) NOPASSWD: $SUDOS_DIR/readenvironment.sh" > /etc/sudoers.d/docker2
+   echo "$SUDO_USER $(/bin/hostname)=(root) NOPASSWD: $BIN_DIR/readenvironment.sh" > /etc/sudoers.d/docker2
    /usr/bin/env | grep "^REV_" > "$RUNTIME_ENVIRONMENT"
 fi
-unset password_$SUDO_USER
-exec /usr/bin/env -i "$SUDOS_DIR/initsamba.sh"
+if [ -n "$REV_password_$SUDO_USER" ]
+then
+   unset "$REV_password_$SUDO_USER"
+fi
+exec /usr/bin/env -i "$BIN_DIR/initandrun.sh"
