68b11651679a7d1cc58c39adc890d7364e47e1c3
huggla <henrik.uggla@kristianstad.se>
2018-03-02 10:54:57 +0100

    Update and rename bin/sudos/readenvironment.sh to bin/readruntimeenvironment.sh

--- bin/readruntimeenvironment.sh
+++ bin/readruntimeenvironment.sh
@@ -6,7 +6,7 @@ readonly BIN_DIR="$(/usr/bin/dirname $0)"
 readonly RUNTIME_ENVIRONMENT="$BIN_DIR/runtime_environment"
 if [ -f "$RUNTIME_ENVIRONMENT" ]
 then
-   echo "$SUDO_USER $(/bin/hostname)=(root) NOPASSWD: $BIN_DIR/readenvironment.sh" > /etc/sudoers.d/docker2
+   echo "$SUDO_USER $(/bin/hostname)=(root) NOPASSWD: $BIN_DIR/readruntimeenvironment.sh" > /etc/sudoers.d/docker2
    /usr/bin/env | grep "^REV_" > "$RUNTIME_ENVIRONMENT"
 fi
 if [ -n "$REV_password_$SUDO_USER" ]
